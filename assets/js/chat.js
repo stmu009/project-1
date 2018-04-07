2190db5643ccd0a3fd811364d8f0ecb67dc97f6c
Damian <dramirez@petmate.com>
2018-04-07 12:21:09 -0500

    added bootstrap 4 to chat box

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -23,12 +23,13 @@ var startListening = function () {
 
     var msgTextElement = document.createElement("p");
     msgTextElement.textContent = msg.text;
+    msgTextElement.className = "mb-1";
 
     var msgElement = document.createElement("div");
     msgElement.appendChild(msgUsernameElement);
     msgElement.appendChild(msgTextElement);
 
-    msgElement.className = "msg";
+    msgElement.className = "msg list-group-item align-items-start";
     var results = document.getElementById("results");
     results.insertBefore(msgElement, results.firstChild);
   });
