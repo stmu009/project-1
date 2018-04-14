c0a3a720c0feda0e8c5bafb5bbffd5795a426ce0
Damian <dramirez@petmate.com>
2018-04-14 13:55:56 -0500

    removed comments

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -218,29 +218,11 @@ var startListeningScores = function () {
           }
       }
       $('#mq-section').html(marquee)
-    //   console.log("childSnapshot", childSnapshot);
-  
-    //   var msgUsernameElement = document.createElement("b");
-    //   msgUsernameElement.textContent = msg.username;
-  
-    //   var msgTextElement = document.createElement("p");
-    //   msgTextElement.textContent = msg.text;
-    //   msgTextElement.className = "mb-1";
-  
-    //   var msgElement = document.createElement("div");
-    //   msgElement.appendChild(msgUsernameElement);
-    //   msgElement.appendChild(msgTextElement);
-  
-    //   msgElement.className = "msg list-group-item align-items-start";
-    //   var results = document.getElementById("results");
-    //   results.insertBefore(msgElement, results.firstChild);
     });
   };
   
   // Begin listening for data
   startListeningScores();
 
-//transition button back to submit
-//out of time logic
 //add gifs
 
