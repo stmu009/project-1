e0e66b5017c6335d162848eed5110e9a9c8b5c39
Tracer16 <tracermagee@gmail.com>
2018-04-19 17:44:18 -0500

    added CSS

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -246,7 +246,7 @@ var startListeningScores = function () {
             if (userObject.hasOwnProperty(key)) {
                 const element = userObject[key];
                 var span = $('<span>')
-                span.text(`${element.username}: ${element.score}pts;   `)
+                span.text(`${element.username}: ${element.score}pts; ____________  `)
                 $(marquee).append(span);
             }
         }
